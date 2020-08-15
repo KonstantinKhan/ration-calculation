67090efad276962125d8fc082b8871684e74af0b
KonstantinKhan <khan366kos@gmail.com>
2020-08-15 18:22:58 +0500

    Application skeleton created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RationComponent} from './ration/ration.component';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
+import {DishesComponent} from './dishes/dishes.component';
+import {ProductsComponent} from './products/products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: RationComponent},
+      {path: 'ration/:id', component: RationComponent},
+      {path: 'dishes', component: DishesComponent},
+      {path: 'products', component: ProductsComponent}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { RationComponent } from './ration/ration.component';
+import { DishesComponent } from './dishes/dishes.component';
+import { ProductsComponent } from './products/products.component';
+
+registerLocaleData(localeRu, 'ru');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    RationComponent,
+    DishesComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'ru'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
