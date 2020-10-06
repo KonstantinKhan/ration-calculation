888a3c46e9056f82f82783fa03240b1ab4d6ff95
KonstantinKhan <khan366kos@gmail.com>
2020-10-06 11:22:52 +0500

    fixed product display

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,8 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter, HostListener, ViewChild} from '@angular/core';
+import {Product} from '../shared/interfaces';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProductService} from '../shared/services/product.service';
+import {CommonService} from '../shared/services/common.service';
 
 @Component({
   selector: 'app-add-product',
@@ -7,12 +11,64 @@ import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
-  @Input() title = 'title';
+  @ViewChild('addProductForm') addProductForm;
+
+  form: FormGroup;
+
+  @Input() title;
   @Output() close = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private productService: ProductService,
+    private commonService: CommonService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1)]),
+      calories: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1)]),
+      proteins: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1)]),
+      fats: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1)]),
+      carbohydrates: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1)])
+    });
+  }
+
+  submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    const product: Product = {
+      productId: null,
+      name: this.form.value.name,
+      calories: this.form.value.calories,
+      proteins: this.form.value.proteins,
+      fats: this.form.value.fats,
+      carbohydrates: this.form.value.carbohydrates,
+      verified: false,
+      userId: null
+    };
+    console.log(product);
+    this.productService.addProduct(product).subscribe(() => {
+      this.commonService.callNgInit();
+    });
+    this.close.emit();
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event): void {
+    if (!this.addProductForm.nativeElement.contains(event.target)) {
+      this.close.emit();
+    }
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,9 +1,9 @@
 import {Component, ComponentFactoryResolver, OnInit, ViewChild} from '@angular/core';
 import {ProductService} from '../shared/services/product.service';
 import {Product} from '../shared/interfaces';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AddProductComponent} from '../add-product/add-product.component';
 import {RefDirective} from '../ref.directive';
+import {CommonService} from '../shared/services/common.service';
 
 @Component({
   selector: 'app-products',
@@ -13,67 +13,37 @@ import {RefDirective} from '../ref.directive';
 export class ProductsComponent implements OnInit {
 
   products: Product[];
-  isCreate = false;
-  form: FormGroup;
 
   @ViewChild(RefDirective) refDir: RefDirective;
 
   constructor(
     private productService: ProductService,
-    private resolver: ComponentFactoryResolver
+    private resolver: ComponentFactoryResolver,
+    private commonService: CommonService
   ) {
+    this.commonService.invokeEvent.subscribe( value => {
+      // if (value === 'someVal') {
+      //   console.log('Вызов метода')
+        this.ngOnInit()
+      // }
+    })
   }
 
   ngOnInit(): void {
+    console.log('ngOnInit()')
     this.productService.getAll().subscribe((products: Product[]) => {
       this.products = products;
-    });
-    this.form = new FormGroup({
-      name: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1)]),
-      calories: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1)]),
-      proteins: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1)]),
-      fats: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1)]),
-      carbohydrates: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1)])
     });
   }
 
   showNewProduct(): void {
-    this.isCreate = true;
     const modalFactory = this.resolver.resolveComponentFactory(AddProductComponent);
     this.refDir.containerRef.clear();
     const component = this.refDir.containerRef.createComponent(modalFactory);
 
-    component.instance.title = 'Dynamic title';
+    component.instance.title = 'Новый продукт';
     component.instance.close.subscribe(() => {
       this.refDir.containerRef.clear();
     });
-  }
-
-  submit(): void {
-    if (this.form.invalid) {
-      return;
-    }
-    const product: Product = {
-      productId: null,
-      name: this.form.value.name,
-      calories: this.form.value.calories,
-      proteins: this.form.value.proteins,
-      fats: this.form.value.fats,
-      carbohydrates: this.form.value.carbohydrates,
-      verified: false,
-      userId: null
-    };
-    console.log(product);
-    this.productService.addProduct(product).subscribe();
   }
 }
