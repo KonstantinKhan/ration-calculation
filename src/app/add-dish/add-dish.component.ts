e5ceb07424f61e4432f7b65b962c7551b5315741
KonstantinKhan <khan366kos@gmail.com>
2020-10-13 09:48:38 +0500

    improved presentation of the dish

--- src/app/add-dish/add-dish.component.ts
+++ src/app/add-dish/add-dish.component.ts
@@ -64,26 +64,35 @@ export class AddDishComponent implements OnInit {
     this.searchComponent = '';
   }
 
-  serWeightComponent(dishProduct: DishProduct, $event): void {
+  setWeightComponent(dishProduct: DishProduct, $event): void {
     const weightComponent = $event.target.value;
     dishProduct.weight = weightComponent;
     this.updateDish();
   }
 
   updateDish(): void {
+    let allCalories = 0;
+    let allProteins = 0;
+    let allFats = 0;
+    let allCarbohydrates = 0;
     type FuncSum = (value: DishProduct) => void;
     const setWeight: FuncSum = (value): void => {
       this.dish.weightRaw += +value.weight;
-      this.dish.calories += +value.weight * value.product.calories / 100;
-      this.dish.proteins += +value.weight * value.product.proteins / 100;
-      this.dish.fats += +value.weight * value.product.fats / 100;
-      this.dish.carbohydrates += +value.weight * value.product.carbohydrates / 100;
+      allCalories += +value.weight * value.product.calories / 100;
+      allProteins += +value.weight * value.product.proteins / 100;
+      allFats += +value.weight * value.product.fats / 100;
+      allCarbohydrates += +value.weight * value.product.carbohydrates / 100;
+      this.dish.calories = allCalories / this.dish.weightRaw * 100;
+      this.dish.proteins = allProteins / this.dish.weightRaw * 100;
+      this.dish.fats = allFats / this.dish.weightRaw * 100;
+      this.dish.carbohydrates = allCarbohydrates / this.dish.weightRaw * 100;
     };
     this.dish.weightRaw = 0;
     this.dish.calories = 0;
     this.dish.proteins = 0;
     this.dish.fats = 0;
     this.dish.carbohydrates = 0;
+
     this.dish.dish_product.forEach(setWeight);
   }
 
